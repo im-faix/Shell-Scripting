db2f77d8276581d9e1927323fd4bc98b8b556032
im-faix <faexan92@gmail.com>
2025-05-28 20:35:42 +0530

    basic and variables in shellscripting

---
+++ basics/basics.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# The line above is called a "shebang".
+# It tells the system to use the Bash shell to run this script.
+
+# This is a comment. It is ignored by the shell.
+# Comments are used to explain what the script is doing.
+
+# The 'echo' command is used to print text to the terminal.
+# Let's use it to display a message:
+
+echo "Hello Shell-Scripting"
---
+++ basics/variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# A variable is like a container to store data (text, numbers, etc.)
+# You assign a value to a variable like this:
+
+name="im-faix"
+
+# IMPORTANT: No spaces around the equal sign (=)
+
+# To use (reference) a variable, add a dollar sign ($) in front of its name
+
+echo "Welcome, $name!"  # This will print: Welcome, im-faix!
