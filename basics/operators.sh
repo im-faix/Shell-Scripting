e573c5db7675dd39993e309d4f180de050fda509
im-faix <faexan92@gmail.com>
2025-05-28 20:44:01 +0530

    operators in shell scripting

---
+++ basics/operators.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# This script demonstrates how to use Operators 
+
+
+
+echo "Arithmetic Operators"
+# | + - * / % |
+a=10
+b=2
+
+# Inorder to perform operators use this syntax $((value1 operator value2))
+echo "Addition of $a and $b is " $((a+b))   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
